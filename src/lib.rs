73a3f4a19893d091f57e95a5b3052a25587c73ab
nnazo <jacobcurtis2266@gmail.com>
2022-05-19 18:08:48 -0500

    feat(rt): add smol runtime

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #[cfg(feature = "async-std")]
 use async_std1 as async_std;
 use futures_lite::future::Future;
+#[cfg(feature = "smol")]
+use smol_timeout::TimeoutExt;
 use std::collections::HashMap;
 use std::sync::Arc;
 use std::time::Duration;
-#[cfg(feature = "tokio")]
-use std::time::Instant;
 use thiserror::Error;
 #[cfg(feature = "tokio")]
 use tokio1 as tokio;
@@ -20,8 +20,7 @@ pub enum BulkheadError {
         #[cfg(feature = "tokio")]
         #[from]
         tokio::time::error::Elapsed,
-        #[cfg(not(feature = "tokio"))]
-        #[cfg(feature = "async-std")]
+        #[cfg(all(not(feature = "tokio"), feature = "async-std"))]
         #[from]
         async_std::future::TimeoutError,
     ),
@@ -33,34 +32,49 @@ pub struct BulkheadConfig {
     max_wait_duration: Duration,
 }
 
+impl BulkheadConfig {
+    pub fn with_max_concurrent_calls(&mut self, max_concurrent_calls: usize) -> &mut Self {
+        self.max_concurrent_calls = max_concurrent_calls;
+        self
+    }
+
+    pub fn with_max_wait_duration(&mut self, max_wait_duration: Duration) -> &mut Self {
+        self.max_wait_duration = max_wait_duration;
+        self
+    }
+}
+
+impl Default for BulkheadConfig {
+    fn default() -> Self {
+        Self {
+            max_concurrent_calls: 25,
+            max_wait_duration: Duration::from_millis(1),
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Bulkhead {
     #[cfg(feature = "tokio")]
     max_concurrent_calls: Arc<tokio::sync::Semaphore>,
-    #[cfg(not(feature = "tokio"))]
-    #[cfg(feature = "async-std")]
+    #[cfg(all(not(feature = "tokio"), any(feature = "async-std", feature = "smol")))]
     max_concurrent_calls: Arc<async_lock::Semaphore>,
     max_wait_duration: Duration,
 }
 
 impl Bulkhead {
-    pub fn new(max_concurrent_calls: usize, max_wait_duration: Duration) -> Self {
-        Self {
-            #[cfg(feature = "tokio")]
-            max_concurrent_calls: Arc::new(tokio::sync::Semaphore::new(max_concurrent_calls)),
-            #[cfg(not(feature = "tokio"))]
-            #[cfg(feature = "async-std")]
-            max_concurrent_calls: Arc::new(async_lock::Semaphore::new(max_concurrent_calls)),
-            max_wait_duration,
-        }
-    }
-
-    pub fn from_config(config: BulkheadConfig) -> Self {
+    pub fn new(config: BulkheadConfig) -> Self {
         let BulkheadConfig {
             max_concurrent_calls,
             max_wait_duration,
         } = config;
-        Self::new(max_concurrent_calls, max_wait_duration)
+        Self {
+            #[cfg(feature = "tokio")]
+            max_concurrent_calls: Arc::new(tokio::sync::Semaphore::new(max_concurrent_calls)),
+            #[cfg(all(not(feature = "tokio"), any(feature = "async-std", feature = "smol")))]
+            max_concurrent_calls: Arc::new(async_lock::Semaphore::new(max_concurrent_calls)),
+            max_wait_duration,
+        }
     }
 
     #[cfg(feature = "tokio")]
@@ -68,21 +82,31 @@ impl Bulkhead {
     where
         F: Future<Output = R>,
     {
-        let now = Instant::now();
-        let until = (now + self.max_wait_duration).into();
         let permit_fut = self.max_concurrent_calls.acquire();
-        let _permit = tokio::time::timeout_at(until, permit_fut).await??;
+        let _permit = tokio::time::timeout(self.max_wait_duration, permit_fut).await??;
         Ok(f.await)
     }
 
-    #[cfg(not(feature = "tokio"))]
-    #[cfg(feature = "async-std")]
+    #[cfg(all(not(any(feature = "tokio", feature = "smol")), feature = "async-std"))]
     pub async fn limit<F, R>(&self, f: F) -> Result<R, BulkheadError>
     where
         F: Future<Output = R>,
     {
         let permit_fut = self.max_concurrent_calls.acquire();
         let _permit = async_std::future::timeout(self.max_wait_duration, permit_fut).await?;
+        Ok(f.await)
+    }
+
+    #[cfg(all(not(any(feature = "tokio", feature = "async-std")), feature = "smol"))]
+    pub async fn limit<F, R>(&self, f: F) -> Result<R, BulkheadError>
+    where
+        F: Future<Output = R>,
+    {
+        let permit_fut = self.max_concurrent_calls.acquire();
+        let _permit = permit_fut
+            .timeout(self.max_wait_duration)
+            .await
+            .ok_or(BulkheadError::Timeout())?;
         Ok(f.await)
     }
 }
